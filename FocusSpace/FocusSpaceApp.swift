92fdbef6aeb2c60a7336e5b3086388136cf6541a
Booya <booya.pannachai@gmail.com>
2025-08-22 21:46:37 +0700

    Initial Commit

---
+++ FocusSpace/FocusSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusSpaceApp.swift
+//  FocusSpace
+//
+//  Created by Panachai Sulsaksakul on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
